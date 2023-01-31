c5ba659fbbebd934bb0820552165a0d9fdda1a3b
Rider <1023007219@qq.com>
2023-01-31 11:31:33 +0800

    feat(custom):

--- .cz-config.cjs
+++ .cz-config.cjs
@@ -1,13 +1,13 @@
 module.exports = {
     disableEmoji: false,
-    format: '{type}{scope}: {emoji}{subject}',
+    format: 'fuck: {type}{scope}: {emoji}{subject}',
     list: [
         'feat',
         'fix',
         'docs',
         'style',
         'refactor',
-        'perf',
+        ' perf',
         'test',
         'chore',
         'revert',
@@ -19,22 +19,22 @@ module.exports = {
     minMessageLength: 3,
     questions: ['type', 'scope', 'subject', 'body', 'breaking', 'issues', 'lerna'],
     // 跳过问题
-    skipQuestions: ['body', 'footer'],
+    // skipQuestions: ['body', 'footer'],
     scopes: [],
     // 可选类型
     types: [
-        {value: 'feat', description: 'feat:     新功能', emoji: '🎉',},
-        {value: 'fix', description: 'fix:      修复', emoji: '🪲',},
-        {value: 'docs', description: 'docs:     文档变更', emoji: '📝',},
-        {value: 'style', description: 'style:    代码格式(不影响代码运行的变动)', emoji: '🎨',},
-        {value: 'refactor', description: 'refactor: 重构(对代码和项目结构的重构)', emoji: '💡',},
-        {value: 'perf', description: 'perf:     性能优化', emoji: '⚡️',},
-        {value: 'test', description: 'test:     增加测试', emoji: '📌',},
-        {value: 'chore', description: 'chore:    构建过程或辅助工具的变动', emoji: '🤖',},
-        {value: 'revert', description: 'revert:   回退', emoji: '🔙'},
-        {value: 'build', description: 'build:    打包', emoji: '🛠️'},
-        {value: 'ci', description: 'ci:    与CI有关的变化', emoji: '🎡'},
-        {value: 'release', description: 'release:    创建一个发布提交', emoji: '🚩',},
+        {value: 'feat', name: '✨  feat:     新功能', emoji: '✨',},
+        {value: 'fix', name: '🐞  fix:      修复', emoji: '🪲',},
+        {value: 'docs', name: '📚  docs:     文档变更', emoji: '📝',},
+        {value: 'style', name: '🎨  style:    代码格式(不影响代码运行的变动)', emoji: '🎨',},
+        {value: 'refactor', name: '💡  refactor: 重构(对代码和项目结构的重构)', emoji: '💡',},
+        {value: 'perf', name: '⚡️  perf:     性能优化', emoji: '⚡️',},
+        {value: 'test', name: '📌  test:     增加测试', emoji: '📌',},
+        {value: 'chore', name: '🤖  chore:    构建过程或辅助工具的变动', emoji: '🤖',},
+        {value: 'revert', name: '🔙  revert:   回退', emoji: '🔙'},
+        {value: 'build', name: '🛠️  build:    打包', emoji: '🛠️'},
+        {value: 'ci', name: '🎡  ci:    与CI有关的变化', emoji: '🎡'},
+        {value: 'release', name: '🚩  release:    创建一个发布提交', emoji: '🚩',},
     ],
     // 消息步骤
     messages: {
@@ -42,7 +42,7 @@ module.exports = {
         scope: '请输入修改范围(可选):',
         subject: '请简要描述提交(必填):',
         body: '请输入详细描述(可选):',
-        footer: '请输入要关闭的issue(可选):',
+        footer: '请输入要关闭的issue，例如："fix #123" "re #123" (可选):',
         confirmCommit: '确认使用以上信息提交？(y/n/e/h)'
     },
     // subject文字长度默认是72
