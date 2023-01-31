4546e9105f543a0d778fc78b243236d9674f702a
Rider <1023007219@qq.com>
2023-01-31 12:15:30 +0800

    feat(package.json): 配置git提交规范
    团队需要按照这个规范提交
    
    BREAKING CHANGE: 大版本更新
    
    fix #123

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,16 +1,11 @@
 module.exports = {
-    extends: [
-        '@commitlint/config-conventional' // scoped packages are not prefixed
-    ],
-     rules: {
-    'type-enum': [
-      2,
-      'always',
-      ['feat', 'fix', 'refactor', 'docs', 'test', 'style', 'chore', 'revert'],
-    ],
-    'type-case': [2, 'always', ['lower-case', 'upper-case']],
-    'scope-case': [0, 'never'],
-    'subject-case': [0, 'never'],
-    'scope-empty': [2, 'never'],
-  },
+    // scoped packages are not prefixed
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [2, 'always', ['feat', 'fix', 'refactor', 'docs', 'test', 'style', 'chore', 'revert']],
+        'type-case': [2, 'always', ['lower-case', 'upper-case']],
+        'scope-case': [0, 'never'],
+        'subject-case': [0, 'never'],
+        'scope-empty': [2, 'never'],
+    },
 }
